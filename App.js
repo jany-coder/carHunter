f094931b6ca61917384231e014ff6a75880ae5f9
jany-coder <jany_ete@yahoo.com>
2021-06-04 00:56:53 +0600

    final project

--- App.js
+++ App.js
@@ -1,19 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
-import { Header } from 'react-native/Libraries/NewAppScreen';
-import CarItem from './components/CarItem';
+import { StyleSheet, View } from 'react-native';
+import CarsList from './components/CarsList';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Header />
-      <CarItem></CarItem>
-    <StatusBar style="auto" />
-  </View>
-);
+      <CarsList />
+      <StatusBar style="auto" />
+    </View>
+  );
 }
-
 
 const styles = StyleSheet.create({
   container: {
@@ -22,36 +21,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-
-  carContainer: {
-    width: '100%',
-    height: '100%',
-  },
-
-  titles: {
-    marginTop: '30%',
-    width: '100%',
-    alignItems: 'center'
-  },
-
-  title: {
-    fontSize: 40,
-    fontWeight: '500'
-
-  },
-
-  subtitle: {
-    fontSize: 16,
-    color: '#5c5e62'
-  },
-
-  image: {
-    width: '100%',
-    height: '100%',
-    resizeMode: 'cover',
-    position: 'absolute'
-
-  }
-
 });
-
